7c89e9691daa08c32955c51a69b90c4d779ee5cd
bimkon <lexakutsenko@mail.ru>
2021-08-02 15:20:22 +0400

    Обновил формулу #6

--- src/plugin/typeguards/typeguards.ts
+++ src/plugin/typeguards/typeguards.ts
@@ -1,3 +1,3 @@
 export const isBoolean = (value: unknown): value is boolean => typeof value === 'boolean';
 
-export const isNumber = (value: unknown): value is number => typeof value === 'number' && !Number.isNaN(value) && value >= 0;
+export const isNumber = (value: unknown): value is number => typeof value === 'number' && !Number.isNaN(value);
