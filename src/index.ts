ade5815375554126eaa10de34cabcfd3428434a5
bimkon <lexakutsenko@mail.ru>
2021-12-24 17:37:22 +0400

    fix naming

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-function importAll(resolve: any) {
-  resolve.keys().forEach(resolve);
-}
-importAll(require.context('../src/', true, /\.ts$|\.scss$/));
--- src/plugin/types.ts
+++
@@ -1,6 +0,0 @@
-export type Axis = {
-  direction: 'top' | 'left';
-  eventClientOrientation: 'clientY' | 'clientX';
-  offsetParameter: 'offsetHeight' | 'offsetWidth';
-  styleOrientation:'height' | 'width';
-};
